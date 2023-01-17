bbc6fc782acc86aad420d754aae92a7e4a1626be
Simen Kristiansen <simenbolstad@gmail.com>
2023-01-17 09:49:20 +0100

    Initial commit

---
+++ Match.cs
@@ -0,0 +1,24 @@
+﻿namespace Tippekupong;
+
+public class Match
+{
+    public int ScoreHome;
+    public int ScoreAway;
+    public bool IsActive = true;
+    public void Goal(string team)
+    {
+        switch (team)
+        {
+            case "H":
+                ScoreHome++;
+                break;
+            case "B":
+                ScoreAway++;
+                break;
+        }
+    }
+    public void EndGame()
+    {
+        IsActive = false;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Tippekupong;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Gyldig tips: \r\n - H, U, B\r\n - halvgardering: HU, HB, UB\r\n - helgardering: HUB\r\nHva har du tippet for denne kampen? ");
+        var bet = Console.ReadLine();
+        var match = new Match();
+        while (match.IsActive)
+        {
+            Console.Write("Kommandoer: \r\n - H = scoring hjemmelag\r\n - B = scoring bortelag\r\n - X = kampen er ferdig\r\nAngi kommando: ");
+            string? command = Console.ReadLine()?.ToUpper();
+            match.IsActive = command != "X";
+            switch (command)
+            {
+                case "H":
+                    match.Goal(command);
+                    break;
+                case "B":
+                    match.Goal(command);
+                    break;
+            }
+            Console.WriteLine($"Stillingen er {match.ScoreHome}-{match.ScoreAway}.");
+        }
+
+        var result = match.ScoreHome == match.ScoreAway ? "U" : match.ScoreHome > match.ScoreAway ? "H" : "B";
+        var isBetCorrect = bet.Contains(result);
+        var isBetCorrectText = isBetCorrect ? "riktig" : "feil";
+        Console.WriteLine($"Du tippet {isBetCorrectText}");
+    }
+}
